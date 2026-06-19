0e887e56de26dab65d3402b77b6dfd755e22ac56
Antoine Saez Dumas <antoine.saezdumas.git@gmail.com>
2026-06-19 11:40:18 +0200

    build: generate a version.h header instead of adding compile definitions
    the compile defintions caused every source file to get recompiled each
    time a commit was made instead of just default.cpp

--- src/cmds/default.cpp
+++ src/cmds/default.cpp
@@ -8,16 +8,12 @@
 #include "oly/log.hpp"
 #include "oly/utils.hpp"
 
+#include "../version.h"
+
 void Default::print_version() {
-#if defined(OLY_NAME) && defined(OLY_VERSION) && defined(OLY_BUILD_TYPE) &&              \
-    defined(OLY_COMMIT_HASH)
 	std::println("{} version {}", OLY_NAME, OLY_VERSION);
 	std::println("Build type: {}", OLY_BUILD_TYPE);
 	std::println("Commit hash: {}", OLY_COMMIT_HASH);
-#else
-	static_assert(false, "ERROR: OLY_NAME, OLY_VERSION and OLY_BUILD_TYPE preprocessor "
-	                     "macros must be defined");
-#endif
 }
 
 Default::Default() {
